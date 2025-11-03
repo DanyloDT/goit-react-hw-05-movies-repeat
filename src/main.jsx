4017c3536b766a72c4ef4f1fbeb1a6b20d70a100
Danylo Diachenko <danylodiachenko@gmail.com>
2025-11-03 22:38:20 +0200

    fix basename

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies-repeat">
       <App />
     </BrowserRouter>
   </StrictMode>
